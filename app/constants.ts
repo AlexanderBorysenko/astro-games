ec1ef569d866550cc84b90228867dd8d27920fde
AlexanderBorysenko <borysenko.alexander@gmail.com>
2025-08-01 16:08:02 +0300

    fix: correct Telegram and X links in constants.ts

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,3 @@
-export const TELEGRAM_LINK = 'https://t.me/astro_games';
-export const X_LINK = 'https://t.me/AstroGameWeb3';
+export const TELEGRAM_LINK = 'https://t.me/AstroGameWeb3';
+export const X_LINK = 'x.com/astrogamesolana';
 export const GAME_LINK = '/planes';
