8dc74b9afd5cab9c538f604797a6fec351030eae
AlexanderBorysenko <borysenko.alexander@gmail.com>
2025-08-01 22:52:04 +0300

    fix: add missing protocol to X link in constants.ts

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,3 @@
 export const TELEGRAM_LINK = 'https://t.me/AstroGameWeb3';
-export const X_LINK = 'x.com/astrogamesolana';
+export const X_LINK = 'https://x.com/astrogamesolana';
 export const GAME_LINK = '/planes';
